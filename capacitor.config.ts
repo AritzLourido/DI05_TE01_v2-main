dd1fea5c68f2970d33347cd7f63b0650e54c8745
AritzLourido <aritz.lourido@outlook.com>
2024-06-02 22:13:36 +0200

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DI05_TE01_v2',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
